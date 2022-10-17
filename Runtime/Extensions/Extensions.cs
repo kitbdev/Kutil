690b0a2d461102e9be6c234b3afc3a4db60989f7
kit <kitbdev@gmail.com>
2022-10-17 15:11:53 -0400

    more boundsint ext, selbase monobehaivior

--- Runtime/Extensions/Extensions.cs
+++ Runtime/Extensions/Extensions.cs
@@ -1,5 +1,9 @@
 using System.Collections;
 using UnityEngine;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Kutil {
     public static class Extentions {
@@ -73,6 +77,28 @@ namespace Kutil {
         public static bool ContainsBounds(this BoundsInt bounds, BoundsInt smaller) {
             return bounds.Contains(smaller.min) && bounds.Contains(smaller.max);
         }
+        // /// <summary>
+        // /// Returns true if this BoundsInt contains another entirely
+        // /// </summary>
+        // public static bool ContainsInclusive(this BoundsInt bounds, Vector3Int point) {
+        //     return bounds.Contains(point)||bounds;
+        // }
+        /// <summary>
+        /// Returns true if the point is on the boundaries of the BoundsInt.
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public static bool IsOnBorder(this BoundsInt bounds, Vector3Int point, bool inclusive = false) {
+            int inc = inclusive ? -1 : 0;
+            return
+                point.x == bounds.xMin ||
+                point.x == bounds.xMax + inc ||
+                point.y == bounds.yMin ||
+                point.y == bounds.yMax + inc ||
+                point.z == bounds.zMin ||
+                point.z == bounds.zMax + inc;
+        }
         /// <summary>
         /// Returns the Euclidean distance to the point from the closest point on the border.
         /// negative if interior
@@ -94,14 +120,16 @@ namespace Kutil {
         /// <param name="point"></param>
         /// <returns></returns>
         public static Vector3Int ClosestPoint(this BoundsInt bounds, Vector3Int point) {
-            Vector3Int closestPoint = point;
-            if (point.x < bounds.xMin) closestPoint.x = bounds.xMin;
-            else if (point.x > bounds.xMax) closestPoint.x = bounds.xMax;
-            if (point.y < bounds.yMin) closestPoint.y = bounds.yMin;
-            else if (point.y > bounds.yMax) closestPoint.y = bounds.yMax;
-            if (point.z < bounds.zMin) closestPoint.z = bounds.zMin;
-            else if (point.z > bounds.zMax) closestPoint.z = bounds.zMax;
-            return closestPoint;
+            // Vector3Int closestPoint = point;
+            // if (point.x < bounds.xMin) closestPoint.x = bounds.xMin;
+            // else if (point.x > bounds.xMax) closestPoint.x = bounds.xMax;
+            // if (point.y < bounds.yMin) closestPoint.y = bounds.yMin;
+            // else if (point.y > bounds.yMax) closestPoint.y = bounds.yMax;
+            // if (point.z < bounds.zMin) closestPoint.z = bounds.zMin;
+            // else if (point.z > bounds.zMax) closestPoint.z = bounds.zMax;
+            // return closestPoint;
+            point.Clamp(bounds.min, bounds.max);// - Vector3Int.one
+            return point;
         }
         /// <summary>
         /// Returns the closest point on the box. not inside the box.
@@ -111,12 +139,7 @@ namespace Kutil {
         /// <returns></returns>
         static Vector3Int ClosestPointOnBorder(this BoundsInt bounds, Vector3Int point) {
             Vector3Int closestPoint = bounds.ClosestPoint(point);
-            if (point.x == bounds.xMin ||
-                point.x == bounds.xMax ||
-                point.y == bounds.yMin ||
-                point.y == bounds.yMax ||
-                point.z == bounds.zMin ||
-                point.z == bounds.zMax) {
+            if (bounds.IsOnBorder(closestPoint)) {
                 return closestPoint;
             }
             // find the nearest axis
@@ -143,6 +166,40 @@ namespace Kutil {
             return closestPoint;
         }
 
+        public static Vector3Int[] CornerPositions(this BoundsInt bounds) {
+            return new Vector3Int[]{
+                new Vector3Int(bounds.xMin, bounds.yMin, bounds.zMin),
+                new Vector3Int(bounds.xMax, bounds.yMin, bounds.zMin),
+                new Vector3Int(bounds.xMin, bounds.yMax, bounds.zMin),
+                new Vector3Int(bounds.xMax, bounds.yMax, bounds.zMin),
+                new Vector3Int(bounds.xMin, bounds.yMin, bounds.zMax),
+                new Vector3Int(bounds.xMax, bounds.yMin, bounds.zMax),
+                new Vector3Int(bounds.xMin, bounds.yMax, bounds.zMax),
+                new Vector3Int(bounds.xMax, bounds.yMax, bounds.zMax),
+            };
+        }
+        public static void DrawGizmosBounds(this BoundsInt bounds) {
+            Vector3[] poses = bounds.CornerPositions().Select(p => (Vector3)p).ToArray();
+            DrawCube(poses);
+        }
+
+        public static void DrawCube(Vector3[] poses) {
+#if UNITY_EDITOR
+            Handles.DrawLine(poses[0], poses[1]);// bottom square
+            Handles.DrawLine(poses[0], poses[2]);
+            Handles.DrawLine(poses[2], poses[3]);
+            Handles.DrawLine(poses[1], poses[3]);
+            Handles.DrawLine(poses[0], poses[4]);// vertical lines
+            Handles.DrawLine(poses[1], poses[5]);
+            Handles.DrawLine(poses[2], poses[6]);
+            Handles.DrawLine(poses[3], poses[7]);
+            Handles.DrawLine(poses[4], poses[5]);// top square
+            Handles.DrawLine(poses[4], poses[6]);
+            Handles.DrawLine(poses[6], poses[7]);
+            Handles.DrawLine(poses[5], poses[7]);
+#endif
+        }
+
         // copy bounds functionality
         // https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Geometry/Bounds.cs
         // todo rewrite for faster performance?
@@ -159,19 +216,30 @@ namespace Kutil {
         /// Grows the bounds to include the point.
         /// </summary>
         /// <param name="newPoint"></param>
-        public static void Encapsulate(this BoundsInt bounds, Vector3Int newPoint) {
+        public static void Encapsulate(this ref BoundsInt bounds, Vector3Int newPoint) {
             // var b = bounds.AsBounds();
             // b.Encapsulate(newPoint);
             // var bi = b.AsBoundsInt();
             // bounds.SetMinMax(bi.min, bi.max);
-
+            // Debug.Log($"bounds encapsulate o:{bounds} p:{newPoint} min:{Vector3Int.Min(bounds.min, newPoint)}, max:{Vector3Int.Max(bounds.max, newPoint)}");
             bounds.SetMinMax(Vector3Int.Min(bounds.min, newPoint), Vector3Int.Max(bounds.max, newPoint));
+            // Debug.Log($"new bounds:{bounds}");
+        }
+        /// <summary>
+        /// Grows the bounds to include the point. Inclusive
+        /// </summary>
+        /// <param name="newPoint"></param>
+        public static void EncapsulateInclusive(this ref BoundsInt bounds, Vector3Int newPoint) {
+            // Debug.Log($"bounds encapsulate o:{bounds} p:{newPoint} min:{Vector3Int.Min(bounds.min, newPoint + Vector3Int.one)}, max:{Vector3Int.Max(bounds.max, newPoint)}");
+            //? call it inflated instead of inclusive?
+            bounds.SetMinMax(Vector3Int.Min(bounds.min, newPoint), Vector3Int.Max(bounds.max, newPoint + Vector3Int.one));
+            // Debug.Log($"new bounds:{bounds}");
         }
         /// <summary>
         /// Expand the bounds by increasing its size by amount along each side
         /// </summary>
         /// <param name="amount"></param>
-        public static void Expand(this BoundsInt bounds, int amount) {
+        public static void Expand(this ref BoundsInt bounds, int amount) {
             // var b = bounds.AsBounds();
             // b.Expand(amount);
             // var bi = b.AsBoundsInt();
@@ -182,7 +250,7 @@ namespace Kutil {
         /// Expand the bounds by increasing its size by amount along each side
         /// </summary>
         /// <param name="amount"></param>
-        public static void Expand(this BoundsInt bounds, Vector3Int amount) {
+        public static void Expand(this ref BoundsInt bounds, Vector3Int amount) {
             // var b = bounds.AsBounds();
             // b.Expand(amount);
             // var bi = b.AsBoundsInt();
@@ -203,6 +271,23 @@ namespace Kutil {
                     UnityEngine.Object.DestroyImmediate(t.GetChild(i).gameObject);
                 }
             }
+        }
+        //https://answers.unity.com/questions/361275/cant-convert-bounds-from-world-coordinates-to-loca.html
+        public static Bounds TransformBounds(this Transform _transform, Bounds _localBounds) {
+            var center = _transform.TransformPoint(_localBounds.center);
+
+            // transform the local extents' axes
+            var extents = _localBounds.extents;
+            var axisX = _transform.TransformVector(extents.x, 0, 0);
+            var axisY = _transform.TransformVector(0, extents.y, 0);
+            var axisZ = _transform.TransformVector(0, 0, extents.z);
+
+            // sum their absolute value to get the world extents
+            extents.x = Mathf.Abs(axisX.x) + Mathf.Abs(axisY.x) + Mathf.Abs(axisZ.x);
+            extents.y = Mathf.Abs(axisX.y) + Mathf.Abs(axisY.y) + Mathf.Abs(axisZ.y);
+            extents.z = Mathf.Abs(axisX.z) + Mathf.Abs(axisY.z) + Mathf.Abs(axisZ.z);
+
+            return new Bounds { center = center, extents = extents };
         }
     }
     public static class QuaternionExt {
